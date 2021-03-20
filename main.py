7d25a3cc8b89afc1ca554beea035aa266f6db11d
Víctor <victor.martin84@gmail.com>
2021-03-20 20:02:32 +0100

    Adding adafruit reporting

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import colorsys
 import os
 import sys
 import ST7735
+import logging
 try:
     # Transitional fix for breaking change in LTR559
     from ltr559 import LTR559
@@ -20,17 +21,49 @@ from PIL import ImageDraw
 from PIL import ImageFont
 from fonts.ttf import RobotoMedium as UserFont
 from datetime import datetime
-import logging
+from Adafruit_IO import Client, Feed
 
 TEMPERATURE_FACTOR= 2.25
+FEEDS={
+    'temperature': {
+        'name': 'temperature',
+        'unit_type': 'temperature',
+        'unit_symbol': 'ºC'
+    }, 
+    'humidity': {
+        'name': 'humidity',
+        'unit_type': 'humidity',
+        'unit_symbol': '%'
+    }, 
+    'light': {
+        'name': 'light',
+        'unit_type': 'light',
+        'unit_symbol': 'lux'
+    }
+}
 
 # BME280 temperature/pressure/humidity sensor
 bme280 = BME280()
 
+# Loggin basic configuration
 logging.basicConfig(
     format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s',
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
+
+# Starting values
+global_cpu_temps = [get_cpu_temperature()] * 5
+last_recorded_minute = 0
+aio = Client(os.environ['IO_USERNAME'], os.environ['IO_KEY'])
+
+def setup_io_feeds():
+    existing_feeds = aio.feeds()
+    existing_feeds_keys = list(map(lambda feed: feed.key, existing_feeds))
+
+    for feed_name, feed_values in FEEDS.items():
+        if feed_name not in existing_feeds_keys:
+            feed = Feed(name=feed_values['name'], key=feed_values['name'], unit_symbol=feed_values['unit_symbol'],unit_type=feed_values['unit_type'])
+            aio.create_feed(feed)
 
 def log_data(info):
     logging.info("""Logging something""")
@@ -41,9 +74,6 @@ def get_cpu_temperature():
     output, _error = process.communicate()
     return float(output[output.index('=') + 1:output.rindex("'")])
 
-global_cpu_temps = [get_cpu_temperature()] * 5
-last_recorded_minute = 0
-
 def get_current_temperature():
     cpu_temp = get_cpu_temperature()
     cpu_temps = global_cpu_temps[1:] + [cpu_temp]
@@ -52,7 +82,11 @@ def get_current_temperature():
     comp_temp = raw_temp - ((avg_cpu_temp - raw_temp) / TEMPERATURE_FACTOR)
     return comp_temp
 
+def feed_io(key, value):
+    logging.info("> Sending –– {} –– values to adafruit io {:05.2f} *C".format(key, value))
+    aio.send_data(key, value)
 
+setup_io_feeds()
 
 try:
     while True:
@@ -66,7 +100,7 @@ try:
             }
 
             for key, value in current_values.items():
-                logging.info("{}: {:05.2f} *C".format(key, value))                    
+                feed_io(key, value)
              
                     
 # Exit cleanly
