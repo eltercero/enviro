819c3326ea1530abba6ad5f9fe4cf52f0ec0ee65
Víctor <victor.martin84@gmail.com>
2021-03-26 22:49:40 +0100

    Removing compensated temperature and display things that i won't be using

--- main.py
+++ main.py
@@ -17,10 +17,6 @@ except ImportError:
 
 from bme280 import BME280
 from enviroplus import gas
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
-from fonts.ttf import RobotoMedium as UserFont
 from datetime import datetime
 from Adafruit_IO import Client, Feed
 
@@ -62,15 +58,7 @@ logging.basicConfig(
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
 
-# Get the temperature of the CPU for compensation
-def get_cpu_temperature():
-    with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
-        temp = f.read()
-        temp = int(temp) / 1000.0
-    return temp
-
 # Starting values
-global_cpu_temps = [get_cpu_temperature()] * 5
 last_recorded_minute = 0
 aio = Client(os.environ['IO_USERNAME'], os.environ['IO_KEY'])
 
@@ -83,14 +71,6 @@ def setup_io_feeds():
             feed = Feed(name=feed_values['name'], key=feed_values['name'], unit_symbol=feed_values['unit_symbol'],unit_type=feed_values['unit_type'])
             aio.create_feed(feed)
 
-def get_current_temperature():
-    cpu_temp = get_cpu_temperature()
-    cpu_temps = global_cpu_temps[1:] + [cpu_temp]
-    avg_cpu_temp = sum(cpu_temps) / float(len(cpu_temps))
-    raw_temp = bme280.get_temperature()
-    comp_temp = raw_temp - ((avg_cpu_temp - raw_temp) / TEMPERATURE_FACTOR)
-    return comp_temp
-
 def feed_io(key, value):
     logging.info("> Sending –– {} –– values to adafruit io {:05.2f} *C".format(key, value))
     aio.send_data(key, value)
@@ -100,17 +80,13 @@ def formattedMedian(array):
 
 setup_io_feeds()
 
-temperature_array = []
-humidity_array = []
-oxidising_array = []
-reducing_array = []
-nh3_array = []
+temperature_array, humidity_array, oxidising_array, reducing_array, nh3_array = [],[],[],[],[]
 
 try:
     while True:
         gas_data = gas.read_all()
 
-        temperature_array.append(get_current_temperature())
+        temperature_array.append(bme280.get_temperature())
         humidity_array.append(bme280.get_humidity())
         oxidising_array.append(gas_data.oxidising/1000)
         reducing_array.append(gas_data.reducing/1000)
@@ -118,7 +94,6 @@ try:
 
         if last_recorded_minute != datetime.now().time().minute:
             last_recorded_minute = datetime.now().time().minute
-
 
             current_values = {
                 "temperature": formattedMedian(temperature_array),
@@ -128,11 +103,7 @@ try:
                 "nh3": formattedMedian(nh3_array) 
             }
 
-            temperature_array = []
-            humidity_array = []
-            oxidising_array = []
-            reducing_array = []
-            nh3_array = []
+            temperature_array, humidity_array, oxidising_array, reducing_array, nh3_array = [],[],[],[],[]
 
             for key, value in current_values.items():
                 feed_io(key, value)
